23be478fad9a5119e0ae5c2d347cfa91f0a97904
TimeisHere <seatonowensby@gmail.com>
2026-06-06 20:53:42 -0500

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,38 @@
+const CACHE = "snovale-v1";
+const ASSETS = ["/", "/index.html"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
+self.addEventListener("push", e => {
+  const data = e.data?.json() || { title:"SnoVale", body:"You have a new notification" };
+  e.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: "/icon-192.png",
+      badge: "/icon-192.png",
+      vibrate: [200, 100, 200],
+      data: data.url || "/"
+    })
+  );
+});
+
+self.addEventListener("notificationclick", e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow(e.notification.data));
+});
